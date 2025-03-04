c685b0cfdefa7cf0d3fa8a7da67a0d82093756f8
lucasmoraist <luksmnt1101@gmail.com>
2025-03-04 19:51:35 -0300

    feat: criando canal de comunicação

---
+++ src/main/java/com/example/live_chat_ms/controller/LiveChatController.java
@@ -0,0 +1,19 @@
+package com.example.live_chat_ms.controller;
+
+import com.example.live_chat_ms.dto.ChatRequest;
+import com.example.live_chat_ms.dto.ChatResponse;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class LiveChatController {
+
+    @MessageMapping("/new-message")
+    @SendTo("/topic/new-message")
+    public ChatResponse newMessage(ChatRequest request) {
+        return new ChatResponse(HtmlUtils.htmlEscape(request.username() + ": " + request.message()));
+    }
+
+}
---
+++ src/main/java/com/example/live_chat_ms/dto/ChatRequest.java
@@ -0,0 +1,4 @@
+package com.example.live_chat_ms.dto;
+
+public record ChatRequest(String username, String message) {
+}
